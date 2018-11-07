2e159edb9d7eba8363c20b2498ca4d775914ac69
lapandic <ldzenan@student.ethz.ch>
2018-11-07 16:14:19 +0100

    finished tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # planning-exercises
+
+[![CircleCI](https://circleci.com/gh/GITHUB-USERNAME/planning-exercises.svg?style=svg)](https://circleci.com/gh/GITHUB-USERNAME/planning-exercises)
--- test.py
+++ test.py
@@ -2,7 +2,13 @@
 
 def test_my_implementation():
     """ This will be run by nose """
-    from implementation import CarDynamics
+    from implementation import CarDynamics, CarCommands, CarParameters
     from check import check_car_dynamics_correct
     klass = CarDynamics
-    check_car_dynamics_correct(klass)
+    commands = CarCommands
+    params = CarParameters
+    check_car_dynamics_correct(klass,commands,params)
+
+
+if __name__ == '__main__':
+    test_my_implementation()

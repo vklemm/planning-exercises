9e04b25e60d7e1a1989f9d97f46738ee2446f1f1
vklemm <vklemm@student.ethz.ch>
2018-11-14 19:13:41 +0100

    Test Implementation

--- implementation.py
+++ implementation.py
@@ -52,8 +52,8 @@ class CarDynamics(GenericKinematicsSE2):
         check_isinstance(commands, CarCommands)
 
         # Your code comes here!
-        linear = [0,0]
-        angular = 0
+        linear = [commands.linear_velocity, 0]
+        angular = commands.linear_velocity/self.parameters.wheel_distance * np.tan(commands.steering_angle)
         # represent this as se(2)
         commands_se2 = geo.se2_from_linear_angular(linear, angular)
 
